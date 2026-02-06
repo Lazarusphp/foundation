f8a527dd779ce2a63689162c5ce2c3aad91f3288
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-02-06 23:13:53 +0000

    Updated ArrayRules

--- src/Validation/ArrayRules.php
+++ src/Validation/ArrayRules.php
@@ -6,80 +6,171 @@ use LogicException;
 final class ArrayRules
 {
 
-    private array|string|int|null $key = null;
-    private array|string|int|null $in = null;
 
-    private static array $invalidKeys = [];
-    private static array $invalidValues = [];
+
+// ---- Properties ---- //
+
+
+    // Keys and values for Storing
+    private array|string|int|null $keys = null;
+    private array|string|int|null $values = null;
+
+    // Invalid Keys and Values
+    private array $invalidKeys = [];
+    private array $invalidValues = [];
+
+
+
+    // ---- Static Method  Entry Points ---- //
+
+    
+     /**
+      *  public method used as an entry point.
+      *  @method create()
+      *  @static
+      *  @return self
+      */
 
     public static function create():self
     {
         return new self();
     }
 
-    public static function is($value):bool
+
+    // ---- Public Methods ---- //
+
+    private function isArray($array):bool
     {
-        return is_array($value);
+        return is_array($array);
     }
 
-    public static function inArray(array|string|int $values, array $array):bool
+
+        public function hasKeys(array|string|int $keys,?array $array=null)
     {
-        if(is_array($values))
+        if($array !== null)
         {
-            foreach($values as $value)
-            {
-                if (!in_array($value, $array, true)) 
-                {
-                    return false;
-                }            
-            }
-            return true;
+            return $this->validateKeys($keys,$array);
         }
 
-        return in_array($values,$array);
-    }
-
-public static function hasKeys(array|string|int $keys, array $array):bool
-{
-    if (is_array($keys)) {
-        foreach ($keys as $key) {
-            if (!array_key_exists($key, $array)) return false;
-        }
-        return true;
-    }
-    return array_key_exists($keys, $array);
-}
-
-
-    public function key(array|string|int $key):self
-    {
-        if($this->key !== null)
-        {
+        if($this->keys !== null){
             throw new LogicException("Key has already been Set");
         }
 
-        $this->key = $key;
+            $this->keys = $keys;
+            return $this;
+    }
+
+
+    public function hasValues(array|string|int $values, ?array $array=null)
+    {
+        if($array !== null)
+        {
+            return $this->validateValues($values,$array);
+        }
+
+        if($this->values !== null)
+        {
+            throw new LogicException(" Has Values Method  has already been set");
+        }
+
+        $this->values = $values;
         return $this;
     }
 
+
+    /**
+     * @public @method Validate
+     * @return bool
+     * Method used for validating Chain
+     */
     public function validate(array $value):bool
     {
-        if(!self::is($value))
+        if(!$this->isArray($value))
         {
             throw new LogicException("Validation Failed : Value Must be an array");
         }
 
-        if($this->key !== null  && !self::hasKeys($this->key,$value))
+        if($this->values !== null  && !$this->validateValues($this->values,$value))
         {
-            throw new LogicException("Key is not part of the selected Array");
+            throw new LogicException("Values :  ". implode(", ",$this->invalidValues) . " is not part of the selected Array");
+        }
+
+        if($this->keys !== null  && !$this->validateKeys($this->keys,$value))
+        {
+            throw new LogicException("Key or keys : ".implode(", ",$this->invalidKeys)." is not part of the selected Array");
+        }
+        
+        $this->reset();
+        return true;
+    }
+
+
+    
+    // ---- Private Methods ---- //
+    private function validateKeys(array|string|int $keys, array $array):bool
+    {
+        
+         $this->invalidKeys = [];
+        if(is_array($keys))
+        {
+            foreach($keys as $key)
+            {
+                if(!array_key_exists($key,$array)){
+                    $this->invalidKeys[] = $key;
+                }
+            }
+            
+        return empty($this->invalidKeys);
+        }
+
+        if(!array_key_exists($keys,$array))
+        {
+            $this->invalidKeys[] = $keys;
+            return false;
         }
 
         return true;
     }
 
-    public function in(string|int|array $value)
+    private function validateValues(array|string|int $values, array $array)
     {
-        $this->in = $value;
+        $this->invalidValues = [];
+        if(is_array($values))
+        {
+            foreach($values as $value)
+            {
+                if(in_array($value,$array,true) === false)
+                {
+                    $this->invalidValues[] = $value;
+                }
+            }
+
+        return empty($this->invalidValues);
+        }
+
+
+        if(in_array($values,$array,true) === false)
+        {
+            $this->invalidValues[] = $values;
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @method reset
+     * @return self
+     * @property
+     */
+    public function reset():self
+    {
+        $this->invalidKeys = [];
+        $this->invalidValues = [];
+        $this->keys = null;
+        $this->values = null;
         return $this;
     }
+
+
 }
