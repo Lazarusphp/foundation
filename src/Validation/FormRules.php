6818810a5847f0980823e3013034768be1f29ecc
martin bamber <mbamber1986@gmail.com>
2026-02-09 12:33:53 +0000

    Created New Required and match Methods for Form Rules Incorperated InvalidInputException

---
+++ src/Validation/FormRules.php
@@ -0,0 +1,94 @@
+<?php
+namespace LazarusPhp\Foundation\Validation;
+
+use App\Listeners\InvalidInputListener;
+use LazarusPhp\Exceptions\Dispatcher;
+use App\Exceptions\InvalidInputException;
+use LogicException;
+
+class FormRules
+{
+
+    // ---- Properties ---- //
+
+    private bool|null $requiredValue = null;
+    private string|int|null $matchValue = null;
+
+    // ---- Constructor ---- //
+
+    public function __construct()
+    {
+        Dispatcher::registerListener([InvalidInputListener::class]);
+        $this->reset();
+    }
+
+    // ---- Static Instantiation ---- //
+
+    public static function create()
+    {
+        return new self();
+    }
+
+    // ---- Public Methods ---- //
+
+    public function required():self
+    {
+        if($this->requiredValue !== null)
+        {
+            throw new LogicException("Required has Already Been Set");
+        }
+
+        $this->requiredValue = true;
+        return $this;
+    }
+
+    public function match(string|int $input):self
+    {
+        $this->matchValue = $input;
+        return $this;
+    }
+
+    public function reset():self
+    {
+        // Nulify All Values here;
+
+        $properties = get_object_vars($this);
+
+        foreach($properties as $property => $value)
+        {       
+                $this->{$property} = null;
+        }
+        return $this;
+    }
+
+    public function validate($input)
+    {
+        if($this->requiredValue !== null && !$this->validteRequired($input))
+        {
+            throw new InvalidInputException("Required Field");
+        }
+
+        if($this->matchValue !== null)
+        {
+            echo "WE will attempt to match";
+        }
+        return true;
+    }
+
+
+    // --- Private Setters Methods ---- //
+
+    private function validteRequired($input):bool
+    {   
+        if($this->requiredValue === true && empty($input))
+        {   
+            return false;
+        }
+        return true;
+    }
+
+    public function validateMatch($input)
+    { 
+        // validate Both Inputs.
+    }
+}
