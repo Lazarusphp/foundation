2c1422db9f4baae9a2132e3c3f4e4c390f6f36f5
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-02-14 22:51:40 +0000

    Updated StringRules to support lower upper ucfirst and lcFirst

--- src/Validation/StringRules.php
+++ src/Validation/StringRules.php
@@ -3,13 +3,16 @@
 namespace LazarusPhp\Foundation\Validation;
 
 use LogicException;
-use Symfony\Polyfill\Mbstring\Mbstring;
+use LazarusPhp\Foundation\Validation\Enums\StringCases;
+
+
 
 final class StringRules
 {
 
-    protected int $min;
-    protected int $max;
+    protected int $min = 0;
+    protected int $max = PHP_INT_MAX;
+    protected ?StringCases $case = null;
 
     public static function create()
     {
@@ -51,23 +54,78 @@ final class StringRules
         return $this;
     }
 
-    public function validate(string $string)
+    public function validate(string $string):bool
     {
         if (!self::is($string)) {
             throw new LogicException("Validation Failed  : Value is not a string");
         }
-        $min = $this->min ?? 0;
-        $max = $this->max ?? PHP_INT_MAX;
-
-        if ($min > 0 && !self::minLength($string, $min)) {
-            throw new LogicException("Value does not meet the Minimum character length of $min");
+        
+        $string = $this->applyCase($string);
+       
+        if ($this->min > 0 && !self::minLength($string, $this->min)) {
+            throw new LogicException("Value does not meet the Minimum character length of {$this->min}");
         }
 
         // Only enforce max if max < PHP_INT_MAX
-        if ($max < PHP_INT_MAX && !self::maxLength($string, $max)) {
-            throw new LogicException("Value exceeds the Maximum character length of $max");
+        if ($this->max < PHP_INT_MAX && !self::maxLength($string, $this->max)) {
+            throw new LogicException("Value exceeds the Maximum character length of {$this->max}");
         }
 
         return true;
+    }
+
+    private function setCase(StringCases $stringcase)
+    {
+        if($this->case !== null)
+        {
+            throw new LogicException("A Method has alreay been set ");
+        }
+
+        $this->case = $stringcase;
+        return $this;
+    }
+
+    public function upper()
+    {
+        $this->setCase(StringCases::UPPER);
+        return $this;
+    }
+
+    public function lower()
+    {
+        $this->setCase(StringCases::LOWER);
+        return $this;
+    }
+
+    public function lcFirst()
+    {
+        $this->setCase(StringCases::LCFIRST);
+        return $this;
+    }
+
+        public function ucFirst()
+    {
+        $this->setCase(StringCases::UCFIRST);
+        return $this;
+    }
+
+    private function mblclower(string $string):string
+    {
+        $first = mb_substr($string,0,1);
+        $rest = mb_substr($string,1);
+        return mb_strtolower($first) . $rest;
+
+    }
+
+    private function applyCase($string)
+    {;
+        return match($this->case)
+        {
+            StringCases::LOWER => mb_strtolower($string),
+            StringCases::UPPER => mb_strtoupper($string),
+            StringCases::UCFIRST => mb_convert_case($string,MB_CASE_TITLE),
+            StringCases::LCFIRST=> $this->mblclower($string),
+            null => $string,
+        };
     }
 }
