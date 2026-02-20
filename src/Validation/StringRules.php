74b0eac35693ea60c99d73b75bc2bb2b921a5da8
martin bamber <mbamber1986@gmail.com>
2026-02-20 23:40:15 +0000

    Finished v1 of StringRules

--- src/Validation/StringRules.php
+++ src/Validation/StringRules.php
@@ -5,23 +5,28 @@ namespace LazarusPhp\Foundation\Validation;
 use LogicException;
 use LazarusPhp\Foundation\Validation\Enums\StringCases;
 use LazarusPhp\Foundation\ErrorHandler\Errors;
+use LazarusPhp\Foundation\Validation\Traits\ErrorTrait;
+use LazarusPhp\Foundation\Validation\Traits\IntegerTraits;
 
 final class StringRules
 {
 
-    protected int $min = 0;
-    protected int $max = PHP_INT_MAX;
     private Errors $errors;
     protected ?StringCases $case = null;
+    use ErrorTrait;
+    use IntegerTraits;
 
-    public function __construct(?Errors $errors=null)
+
+    // Prevents Being called as an instance and must be called via Create
+    private function __construct(?Errors $errors=null)
     {
             $this->errors = $errors ?? new Errors();
     }
 
     public static function create(?Errors $errors=null)
     {
-        return new self(new Errors($errors));
+        $errors = $errors ?? new Errors();
+        return new self($errors);
     }
 
     public function minLength(string $string, int $min): bool
@@ -35,47 +40,24 @@ final class StringRules
     }
 
 
-    public function min(int $min)
-    {
-        $this->min = $min;
-        return $this;
-    }
-
-    public function max(int $max)
-    {
-        $this->max = $max;
-        return $this;
-    }
-
-    public function error(string $key)
-    {
-        return $this->errors->get($key);
-    }
-
-    public function errors():array
-    {
-        return $this->errors->all();
-    }
-
     public function validate(string $string):bool
     {
 
         $this->errors->reset();
-        if($this->min > $this->max)
-        {
-                $this->errors->add("config","Minimum Value cannot be larger than Max Value");
-        }
-
-        if ($this->min > 0 && !$this->minLength($string, $this->min)) {
-            $this->errors->add("min","Value $string  does not meet Minimum Requirements of {$this->min} Charcters");
+      
+        if ($this->minValue !== null && !$this->minLength($string, $this->minValue)) {
+            $this->errors->add("min","Value $string  does not meet Minimum Requirements of {$this->minValue} Charcters");
         }
 
         // Only enforce max if max < PHP_INT_MAX
-        if ($this->max < PHP_INT_MAX && !$this->maxLength($string, $this->max)) {    
-            $this->errors->add("max","{$string} Exceeds Maxium Required value of {$this->max}");
+        if ($this->maxValue !== null && !$this->maxLength($string, $this->maxValue)) {    
+            $this->errors->add("max","{$string} Exceeds Maxium Required value of {$this->maxValue}");
         }
 
-            return ($this->errors->count()) ? false : true;    
+   if ($this->minValue !== null && $this->maxValue !== null && $this->minValue > $this->maxValue) {
+    $this->errors->add("config", "Minimum value cannot be larger than Maximum value");
+}
+    return $this->isValid();
     }
 
 }
