ddb359892980068ef277862331a4e89de1536996
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-02-24 15:01:18 +0000

    deleted Application Folder

--- src/Application/Interfaces/ProviderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace LazarusPhp\Foundation\Application\Interfaces;
-use LazarusPhp\Foundation\Application\Container;
-
-interface ProviderInterface
-{
-    public function register(Container $c): void;
-}
